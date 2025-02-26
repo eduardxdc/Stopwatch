6a9b52a14dc6967fcb203ff7f1ff64ea9c816c69
eduardxdc <eduardxdc@proton.me>
2025-02-26 18:05:23 -0300

    feat: finishing the menu by getting the option and calculating the time according to the user input

--- Program.cs
+++ Program.cs
@@ -15,16 +15,26 @@ namespace Stopwatch
             Console.WriteLine("Welcome to the stopwatch! Select how long you want to count below:");
             Console.WriteLine("");
 
-            Console.WriteLine("S => Seconds");
-            Console.WriteLine("M => Minutes");
+            Console.WriteLine("S => Seconds = 10s = 10 Seconds");
+            Console.WriteLine("M => Minutes = 1m = 1 Minute");
             Console.WriteLine("E => Exit");
 
             Console.WriteLine("");
             Console.Write("Enter your choice: ");
 
-            string value = Console.ReadLine();
+            string value = Console.ReadLine().ToLower();
+            char type = char.Parse(value.Substring(value.Length - 1, 1));
+            int time = int.Parse(value.Substring(0, value.Length - 1));
 
-            Console.WriteLine($"parabéns você selecionou: {value}");
+            int multiplier = 1;
+
+            if (type == 'm')
+                multiplier = 60;
+
+            if (time == 'e')
+                System.Environment.Exit(0);
+
+            Start(time * multiplier);
         }
 
         static void Start(int time)
@@ -45,7 +55,7 @@ namespace Stopwatch
             Console.Write("Stopwatch Finish!");
             Thread.Sleep(2500);
 
-
+            Menu();
         }
     }
 }
